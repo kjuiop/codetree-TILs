064211017361d8717788403d1a0146734fda6176
JeongIn <41246605+kjuiop@users.noreply.github.com>
2024-02-08 19:02:43 +0900

    Title: 입력받아 계산 4, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/입력받아 계산 4/input-calculate-4.java
+++ 240208/입력받아 계산 4/input-calculate-4.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 55;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
         int answer = a * 2;
         System.out.printf("%d", answer);
     }
