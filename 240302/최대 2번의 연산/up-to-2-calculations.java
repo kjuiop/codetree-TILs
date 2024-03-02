ef588a803d5f6194f6761e098bedecf44a82cc43
JeongIn <41246605+kjuiop@users.noreply.github.com>
2024-03-02 20:49:28 +0900

    Title: 최대 2번의 연산, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240302/최대 2번의 연산/up-to-2-calculations.java
+++ 240302/최대 2번의 연산/up-to-2-calculations.java
@@ -4,24 +4,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        a = calculate(a);
-        a = calculate(a);
+        a = cal1(a);
+        a = cal2(a);
 
         System.out.println(a);
     }
 
-    public static int calculate(int a) {
-        if (a <= 1) {
-            return a;
-        }
-        
+    public static int cal1(int a) {
         if (a % 2 == 0) {
             a = a / 2;
-        } else {
-            a += 1;
+        }
+            
+        return a;
+    }
+
+    public static int cal2(int a) {
+        if (a % 2 == 1) {
+            a = a + 1;
             a = a / 2;
         }
-
+            
         return a;
     }
 }
