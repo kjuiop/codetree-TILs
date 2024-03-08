04d057225143e2bc2d92dd710d5d71258f434c90
JeongIn <41246605+kjuiop@users.noreply.github.com>
2024-03-08 21:43:38 +0900

    Title: 비교 연산, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240308/비교 연산/comparison-operator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        printoutBoolean(a>=b);
+        printoutBoolean(a>b);
+        printoutBoolean(b>=a);
+        printoutBoolean(b>a);
+        printoutBoolean(a==b);
+        printoutBoolean(a!=b);
+    }
+
+    public static void printoutBoolean(boolean t) {
+        if (t) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
